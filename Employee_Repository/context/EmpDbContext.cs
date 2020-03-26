3f1e7108ccd0135b602cee5d7634b6d2d37b66e6
unknown <er.ankitaryan@gmail.com>
2020-03-26 12:32:44 +0530

    [Ankit]Refactor Added DbContext class in Repository

---
+++ Employee_Repository/context/EmpDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using EmployeeModel;
+using System.Data.Entity;
+using System.Text;
+using DbContext = Microsoft.EntityFrameworkCore.DbContext;
+
+namespace Employee_Repository.context
+{
+    public class EmpDbContext : DbContext
+    {
+        public EmpDbContext(DbContextOptions<EmpDbContext> options) : base(options)
+        {
+        }
+        public System.Data.Entity.DbSet<Model_Of_Employee> Employees
+        {
+            get; set;
+        }
+    }
+ 
+}
