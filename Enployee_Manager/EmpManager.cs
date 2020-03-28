cde9aa39df5b5c2ecf302934bb491d587a25df13
unknown <er.ankitaryan@gmail.com>
2020-03-28 11:12:32 +0530

    [Ankit]Refactor. refactor EmpManager class

---
+++ Enployee_Manager/EmpManager.cs
@@ -0,0 +1,43 @@
+﻿using Employee_Repository;
+using EmployeeModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enployee_Manager
+{
+    public class EmpManager : IEmpManager
+    {
+        private readonly IEmployeeRepository emp_Repository;
+
+        public EmpManager(IEmployeeRepository emp_Repository)
+        {
+            this.emp_Repository = emp_Repository;
+        }
+        public Task<int> AddEmployee(Model_Of_Employee employee)
+        {
+            return this.emp_Repository.AddEmployee(employee);
+        }
+
+        public Model_Of_Employee DeleteEmployee(int id)
+        {
+            return this.emp_Repository.DeleteEmployee(id);
+        }
+
+        public IEnumerable<Model_Of_Employee> GetAllEmployees()
+        {
+            return this.emp_Repository.GetAllEmployees();
+        }
+
+        public Model_Of_Employee GetEmployee(int id)
+        {
+            return this.emp_Repository.GetEmployee(id);
+        }
+
+        public Task<int> UpdateEmployee(Model_Of_Employee employeeChanges)
+        {
+            return this.emp_Repository.UpdateEmployee(employeeChanges);
+        }
+    }
+}
---
+++ Enployee_Manager/IEmpManager.cs
@@ -0,0 +1,17 @@
+﻿using EmployeeModel;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enployee_Manager
+{
+    public interface IEmpManager
+    {
+        Model_Of_Employee GetEmployee(int id);
+        IEnumerable<Model_Of_Employee> GetAllEmployees();
+        Task<int>  AddEmployee(Model_Of_Employee employee);
+        Task<int> UpdateEmployee(Model_Of_Employee employeeChanges);
+        Model_Of_Employee DeleteEmployee(int id);
+    }
+}
