a6d208a26d293cf6faa1c49b4cdfe217e4348aed
unknown <er.ankitaryan@gmail.com>
2020-04-03 12:16:19 +0530

    [Ankit]Added.Inch class in QualityMeasurement

---
+++ QualityMeasurement/Inch.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurement
+{
+   public class Inch
+    {
+        private readonly double inch;
+
+        public Inch()
+        {
+        }
+
+        public Inch(double inch)
+        {
+            this.inch = inch;
+        }
+        public double CheckForEqualValue()
+        {
+            return this.inch;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+    }
+}
