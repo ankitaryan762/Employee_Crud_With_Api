c1ae76d8115e07e821b550e3c333f8a5dac26e75
unknown <er.ankitaryan@gmail.com>
2020-04-03 12:28:12 +0530

    [Ankit]Added.test case PerForm_TestCase for inch Value check

--- NUnit-QualityMeasurement/UnitTesting.cs
+++ NUnit-QualityMeasurement/UnitTesting.cs
@@ -82,5 +82,13 @@ namespace NUnit_QualityMeasurement
             Assert.IsTrue(result);
         }
 
+        [Test]
+        public void Perform_Test__Ref_Check()
+        {
+            Inch inch = new Inch(10);
+            double result = inch.CheckForEqualValue();
+            Assert.AreEqual(10, result);
+        }
+
     }
 }
