862dde273f309ca72564ffc6a38229b26819c3bf
unknown <er.ankitaryan@gmail.com>
2020-04-03 13:30:56 +0530

    [Ankit]Added.UnitCheck class in QualityMeasurement

---
+++ QualityMeasurement/UnitCheck.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurement
+{
+   public class UnitCheck
+   { 
+        public string type;
+        public double length;
+        public UnitCheck()
+        {
+
+        }
+
+        public UnitCheck(string type, double length)
+        {
+            this.type = type;
+            this.length = length;
+        }
+
+        public UnitCheck(string type)
+        {
+            this.type = type;
+        }
+
+        public double CheckForEqualValue()
+        {
+            return this.length;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+
+
+    }
+}
