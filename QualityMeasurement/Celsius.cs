9785be3438905d17a74535ed5c4aca463ffbab6c
unknown <er.ankitaryan@gmail.com>
2020-04-04 08:46:53 +0530

    [Ankit]Added.Celsius class in QualityMeasurement

---
+++ QualityMeasurement/Celsius.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurement
+{
+   public class Celsius
+    {
+        private readonly double celsius;
+        public Celsius()
+        {
+        }
+
+        public Celsius(double celsius)
+        {
+            this.celsius = celsius;
+        }
+
+        public double CheckForEqualValue()
+        {
+            return this.celsius;
+        }
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+    }
+}
