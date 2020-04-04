248705fddd77906da796fe6bef7531d0ea89daa2
unknown <er.ankitaryan@gmail.com>
2020-04-04 08:48:20 +0530

    [Ankit]Added.TemparetureConversion class in QualityMeasurement

---
+++ QualityMeasurement/TemparetureConversion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurement
+{
+   public class TemparetureConversion
+    {
+        public double tempareture;
+        public string type;
+        public TemparetureConversion()
+        { 
+        }
+        public TemparetureConversion(string type, double tempareture)
+        {
+            this.tempareture = tempareture;
+            this.type = type;
+        }
+
+        public TemparetureConversion(string type)
+        {
+            this.type = type;
+        }
+
+        public double CheckForEqualValue()
+        {
+            return this.tempareture;
+        }
+
+        public double ConvertCelsiusToFarenheit()
+        {
+            return 32 * tempareture;
+        }
+
+        public double ConvertFarenheitToCelsius() 
+        {
+            return ((double)9 / 5 * tempareture) + 32;
+
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+
+    }
+}
