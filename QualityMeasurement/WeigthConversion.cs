a7cce6bb30975e8a2998d790e2468655843c27f4
unknown <er.ankitaryan@gmail.com>
2020-04-04 08:53:01 +0530

    [Ankit]Added.WeightConversion class in QualityMeasurement

---
+++ QualityMeasurement/WeigthConversion.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurement
+{
+    public class WeigthConversion
+    {
+        public double weigth;
+        public string type;
+        public WeigthConversion() { 
+        }
+        public WeigthConversion(string type,double weigth)
+        {
+            this.weigth = weigth;
+            this.type = type;
+        }
+
+        public WeigthConversion(string type)
+        {
+            this.type = type;
+        }
+
+        public double CheckForEqualValue()
+        {
+            return this.weigth;
+        }
+
+        public double ConvertKiloGramTo_Gram()
+        {
+            return 1000 * weigth;
+        }
+
+        public double ConvertGramToKilogram()
+        {
+            return weigth / 1000;
+        }
+    }
+}
