414191b21e9788f611e23e48d1a140563c316569
unknown <er.ankitaryan@gmail.com>
2020-04-04 08:46:17 +0530

    [Ankit]Added.LengthConversion class in QualityMeasurement

---
+++ QualityMeasurement/LengthConversion.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurement
+{
+    public class LengthConversion
+    {
+        public double length;
+        public string type;
+        public LengthConversion()
+        { }
+        public LengthConversion(string type,double length)
+        {
+            this.length = length;
+            this.type = type;
+        }
+
+        public LengthConversion(string type)
+        {
+            this.type = type;
+        }
+
+        public double CheckForEqualValue()
+        {
+            return this.length;
+        }
+
+        public double ConvertFeetToInch()
+        {
+            return 12 * length;
+        }
+
+        public double ConvertInchToFeet()
+        {
+            return length/12;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+    }
+}
