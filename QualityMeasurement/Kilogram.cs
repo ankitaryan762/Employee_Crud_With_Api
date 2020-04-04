2f8f51734908d1b6cea568902fadf7a0422eae65
unknown <er.ankitaryan@gmail.com>
2020-04-04 08:44:45 +0530

    [Ankit]Added.Kilogram class in QualityMeasurement

---
+++ QualityMeasurement/Kilogram.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurement
+{
+    public class Kilogram
+    {
+        private readonly double kilogram;
+        public Kilogram()
+        {
+        }
+
+        public Kilogram(double kilogram)
+        {
+            this.kilogram = kilogram;
+        }
+
+        public double CheckForEqualValue()
+        {
+            return this.kilogram;
+        }
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+    }
+}
