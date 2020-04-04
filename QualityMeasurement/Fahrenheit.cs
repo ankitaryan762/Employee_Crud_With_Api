9271a9040fb7439d1391c6d2b9796524cfe95866
unknown <er.ankitaryan@gmail.com>
2020-04-04 08:47:32 +0530

    [Ankit]Added.Fahrenheit class in QualityMeasurement

---
+++ QualityMeasurement/Fahrenheit.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace QualityMeasurement
+{
+    public class Fahrenheit
+    {
+        private readonly double fahrenheit;
+        public Fahrenheit()
+        {
+        }
+
+        public Fahrenheit(double fahrenheit)
+        {
+            this.fahrenheit = fahrenheit;
+        }
+
+        public double CheckForEqualValue()
+        {
+            return this.fahrenheit;
+        }
+        public override bool Equals(object obj)
+        {
+            if ((obj == null) || Object.ReferenceEquals(this, obj) || this.GetType().Equals(obj.GetType()))
+                return true;
+
+            return false;
+        }
+    }
+}
