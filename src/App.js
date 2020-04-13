54400a05852abba177e189acf696c18bd74360eb
unknown <er.ankitaryan@gmail.com>
2020-04-13 11:30:04 +0530

    [Ankit]Added. react app projrct with two component these are functional and class component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import {Greet} from './Componenets/Greet'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header"> 
+          <Greet />
+      </header>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Componenets/Greet.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+//function Greet(){
+  //  return <center><h1>Welcome To React Project</h1></center>
+//}
+
+export const Greet =() => <center><h1>Welcome To React Project</h1></center>
+//export default Greet
