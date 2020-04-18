e00e6a07c3cf5fd233e052a3d46d593b4e1ab890
unknown <er.ankitaryan@gmail.com>
2020-04-19 02:35:20 +0530

    [Ankit]Refactor Repository class

--- QuantityMeasurment_Repository/Repository.cs
+++ QuantityMeasurment_Repository/Repository.cs
@@ -1,10 +1,40 @@
-﻿using System;
+﻿using QuantityMeasurement_Model;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace QuantityMeasurment_Repository
 {
-    class Repository
+   public class Repository:IRepository
     {
+        public decimal FeettoInch(Model value)
+        {
+            return value.Feet * 12;
+        }
+
+        public decimal InchToFeet(Model value)
+        {
+            return value.Inch / 12;
+        }
+
+        public decimal GmtoKg(Model value)
+        {
+            return value.Gram / 1000;
+        }
+
+        public decimal MetertoCentimeter(Model value)
+        {
+            return value.Meter * 100;
+        }
+
+        public decimal CentimetertoMeter(Model value)
+        {
+            return value.Centimeter / 100;
+        }
+
+        public decimal KgtoGm(Model value)
+        {
+            return value.KiloGram * 1000;
+        }
     }
 }
