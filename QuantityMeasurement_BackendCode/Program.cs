f3b010496f78bb0c5d0d7ac15580e86b4431113f
unknown <er.ankitaryan@gmail.com>
2020-04-21 13:34:49 +0530

    [Ankit]Added Implementation of Kestrel in Program class

--- QuantityMeasurement_BackendCode/Program.cs
+++ QuantityMeasurement_BackendCode/Program.cs
@@ -14,11 +14,13 @@ namespace QuantityMeasurement_BackendCode
     {
         public static void Main(string[] args)
         {
-            CreateWebHostBuilder(args).Build().Run();
+            BuildWebHost(args).Run();
         }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+        public static IWebHost BuildWebHost(string[] args) =>
+                WebHost.CreateDefaultBuilder(args)
+                .UseKestrel()
+                .UseIISIntegration()
+                    .UseStartup<Startup>()
+                    .Build();
     }
 }
